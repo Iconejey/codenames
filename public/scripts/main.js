a1d6ecec9553bd8013998b20e28aa24a893e65ed
Nicolas Gouwy <43481095+Iconejey@users.noreply.github.com>
2025-03-11 18:40:14 +0000

    Basic app with random word grid

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -3,14 +3,40 @@ $('#out').onclick = signOut;
 
 authFromURL();
 
-async function onAuth() {
-	$('#out').disabled = false;
+// Game class
+class MainApp extends CustomElement {
+	constructor() {
+		super();
+	}
 
-	const data = await getAccountInfo();
-	$('#name').innerText = data.name;
-	$('#email').innerText = data.email;
+	async init() {
+		$('#out').disabled = false;
+
+		const data = await getAccountInfo();
+		$('#auth').innerText = data.name;
+
+		// Load words
+		const words = await fetchJSON('words.json');
+
+		// Create a grid with 24 random words
+		const grid = new Set();
+		while (grid.size < 24) grid.add(words[Math.floor(Math.random() * words.length)]);
+
+		let line = null;
+
+		for (let word of grid) {
+			if ($$('.card').length % 4 === 0) {
+				line = render(html`<div class="line"></div>`);
+				this.appendChild(line);
+			}
+
+			word = word[0].toUpperCase() + word.slice(1);
+			const card = render(html`<div class="card">${word}</div>`);
+			line.appendChild(card);
+		}
+	}
 }
 
-if (userSignedIn()) onAuth();
+defineComponent(html`<main-app />`);
 
-STORAGE.onChange(console.log);
+if (userSignedIn()) $('main-app').init();
